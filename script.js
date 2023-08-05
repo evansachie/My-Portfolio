6bea3dba6f99f426c902ba17ffcedaa1df23a2cc
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-08-05 13:30:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
   // Typing animation script start
   var typed = new Typed(".typing", {
-    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    strings: ["Aspring Full Stack Developer", "ML & AI Enthusiast", "Computer Engineering Student"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
