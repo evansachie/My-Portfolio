2f2302c7459e8a8e745e0386d0504f1a8242663f
Evans Acheampong <evansachie01@gmail.com>
2026-02-28 21:39:45 +0000

    feat: update home & projects

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 (function () {
   "use strict";
 
-  var modal = document.getElementById("resumeModal");
-  var resumeBtns = document.querySelectorAll(".js-resume-btn");
-  var closeBtn = document.querySelector(".modal-close");
-
   function formatTime(date) {
     var h = date.getHours();
     var m = date.getMinutes();
@@ -25,34 +21,6 @@
   if (document.getElementById("footerTime")) {
     setInterval(updateFooterTime, 1000);
   }
-
-  function openModal() {
-    if (modal) {
-      modal.style.display = "flex";
-      document.body.style.overflow = "hidden";
-    }
-  }
-
-  function closeModal() {
-    if (modal) {
-      modal.style.display = "none";
-      document.body.style.overflow = "";
-    }
-  }
-
-  resumeBtns.forEach(function (btn) {
-    btn.addEventListener("click", openModal);
-  });
-
-  if (closeBtn) closeBtn.addEventListener("click", closeModal);
-
-  window.addEventListener("click", function (e) {
-    if (e.target === modal) closeModal();
-  });
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && modal && modal.style.display === "flex") closeModal();
-  });
 
   document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
     anchor.addEventListener("click", function (e) {
